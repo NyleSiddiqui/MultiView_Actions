ba6c34e1adc21c7754003b9552b01fe276a6a77a
Nyle Siddiqui <lewelfez98@gmail.com>
2024-02-15 19:20:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -65,36 +65,14 @@ if __name__ == '__main__':
 
     parser.add_argument('--run_id', dest='run_id', type=str, required=False, help='Please provide an ID for the current run')
 
-    parser.add_argument('--run_description', dest='run_description', type=str, required=False, help='Please description of the run to write to log')
-
     parser.add_argument('--dataset', type=str, required=True, help='Dataset to use.', choices=["ntu_rgbd_120", 'ntu_rgbd_60', "pkummd",  'mergedntupk', 'numa'])
 
-    parser.add_argument('--model_version', type=str, required=True, help='Specify the model version to use for transformer.', 
-                        choices=["baseline", "i3d", "r3d", "r2plus1d", "v1", "v2", 'v3', "vivit", 'v2+backbone', 'v3+backbone', 'v3+D', 'v3_intermediate', 'swin'])
-
-    parser.add_argument('--input_type', type=str, required=True, help='Specify if the input is either RGB or Flow.', choices=["rgb", "ir", "skeleton"])
-
-    parser.add_argument('--input_dim', type=int, default=224, help='Size of the frames in the input clip.')
-
-    parser.add_argument('--num_frames', type=int, default=32, help='Number of frames in the input clip.')
-
+    parser.add_argument('--model_version', type=str, required=True, help='Specify the model to use', 
+                        choices=['v3',])
+    
     group = parser.add_mutually_exclusive_group(required=True)
 
-    group.add_argument('--skip', type=int, help='Number of frames to skip in the input sequence.')
-
-    group.add_argument("--random_skip", action='store_true')
-
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size.')
-
-    parser.add_argument('--steps', type=int, default=1, help='Number of accumulation steps.')
-
-    parser.add_argument('--patch_size', type=int, default=16, help='Size of the patches for the input frames.')
-
-    parser.add_argument('--hidden_dim', type=int, default=512, help='Size of the features in the Transformer Encoder Layer.')
-
-    parser.add_argument('--num_heads', type=int, default=8, help='Number of heads in the encoder layers.')
-
-    parser.add_argument('--num_layers', type=int, default=5, help='Number of layers for encoder/decoder in the transformer model.')
 
     parser.add_argument('--num_epochs', type=int, default=100, help='Number of epochs.')
 
@@ -106,8 +84,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--optimizer', type=str, default='ADAM', help='provide optimizer preference')
 
-    parser.add_argument('--f1_threshold', type=float, default=0.5, help='Probability threshold for computing F1 Score')
-
     parser.add_argument('--checkpoint', type=str, required=False, help='Path to the pre-trained model.')
 
     parser.add_argument('--ema-decay', default=0.999, type=float)
@@ -116,10 +92,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--seed', type=int, default=7, help='Random seed.')
     
-    parser.add_argument('--action_train', type=bool, default=False, help='Train the baselines with action loss or not')
-    
-    parser.add_argument('--contrastive_train', type=bool, default=False, help='Train the baselines with contrastive feature loss or not')
-
     # parse arguments
     args = parser.parse_args()
 
